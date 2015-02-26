c222b779e00e4e1a64ac1d3fade199edb157764c
Ross Riley <riley.ross@gmail.com>
2015-02-26 07:48:57 +0000

    configure memcache session handler

--- bootstrap.php
+++ bootstrap.php
@@ -6,11 +6,30 @@ $configuration->getVerifier()->removeCheck('apache');
 $configuration->verify();
 $app = new Bolt\Application(array('resources'=>$configuration));
 
-// $app['session.storage.handler'] = $app->share(function ($app) {
-//    $memcache = new \Memcached();
-//    $memcache->addServer();
-//     return new Symfony\Component\HttpFoundation\Session\Storage\Handler\MemcachedSessionHandler();
-// });
+$app['session.storage.handler'] = $app->share(function ($app) {
+    
+    // parse config
+    $servers = explode(",", getenv("MEMCACHIER_SERVERS"));
+    for ($i = 0; $i < count($servers); $i++) {
+      $servers[$i] = explode(":", $servers[$i]);
+    }
+    
+    $m = new Memcached("memcached_pool");
+    $m->setOption(Memcached::OPT_BINARY_PROTOCOL, TRUE);
+    $m->setOption(Memcached::OPT_CONNECT_TIMEOUT, 2000);
+    $m->setOption(Memcached::OPT_POLL_TIMEOUT, 2000);
+    $m->setOption(Memcached::OPT_RETRY_TIMEOUT, 2);
+    $m->setSaslAuthData(getenv("MEMCACHIER_USERNAME"), getenv("MEMCACHIER_PASSWORD"));
+
+    if (!$m->getServerList()) {
+      // We use a consistent connection to memcached, so only add in the servers
+      // first time through otherwise we end up duplicating our connections to the
+      // server.
+      $m->addServers($servers);
+    }
+    
+    return new Symfony\Component\HttpFoundation\Session\Storage\Handler\MemcachedSessionHandler($m);
+});
 
 $app->initialize();
 $app->run();
