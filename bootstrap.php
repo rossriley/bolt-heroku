cea204682574b3d6cc75cc4a09be6681440fd500
Ross Riley <riley.ross@gmail.com>
2015-03-06 06:50:27 +0000

    fix memcache setup

--- bootstrap.php
+++ bootstrap.php
@@ -6,5 +6,18 @@ $configuration->getVerifier()->removeCheck('apache');
 $configuration->verify();
 $app = new Bolt\Application(array('resources'=>$configuration));
 
+$mc = new Memcached('mc');
+$mc->setOption(Memcached::OPT_BINARY_PROTOCOL, TRUE);
+$mc->setSaslAuthData( getenv("MEMCACHIER_USERNAME")
+                   , getenv("MEMCACHIER_PASSWORD") );
+
+$servers = explode(",", getenv("MEMCACHIER_SERVERS"));
+foreach ($servers as $s) {
+    $parts = explode(":", $s);
+    $mc->addServer($parts[0], $parts[1]);
+}
+
+
+$app['session.storage.handler'] = new MemcachedSessionHandler($mc);
 $app->initialize();
 $app->run();
