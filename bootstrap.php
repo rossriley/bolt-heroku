210500e4f8e77a6d2729e727688ca1144f7c2bad
Ross Riley <riley.ross@gmail.com>
2015-03-10 07:44:44 +0000

    no pool for memcached

--- bootstrap.php
+++ bootstrap.php
@@ -10,14 +10,20 @@ $app = new Bolt\Application(array('resources'=>$configuration));
 
 $mc = new Memcached();
 $mc->setOption(Memcached::OPT_BINARY_PROTOCOL, TRUE);
+
+$mc = new Memcached('mc');
+$mc->setOption(Memcached::OPT_LIBKETAMA_COMPATIBLE, true);
 $mc->setSaslAuthData( getenv("MEMCACHIER_USERNAME"), getenv("MEMCACHIER_PASSWORD") );
+if (!count($mc->getServerList())) {
+    $servers = explode(",", getenv("MEMCACHIER_SERVERS"));
 
-$servers = explode(",", getenv("MEMCACHIER_SERVERS"));
-
-foreach ($servers as $s) {
-    $parts = explode(":", $s);
-    $mc->addServer($parts[0], $parts[1]);
+    foreach ($servers as $s) {
+        $parts = explode(":", $s);
+        $mc->addServer($parts[0], $parts[1]);
+    }    
 }
+
+
 
 
 $app['session.storage.handler'] = new MemcachedSessionHandler($mc);
